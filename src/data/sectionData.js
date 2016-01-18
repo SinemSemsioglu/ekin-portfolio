0c11fa3b782a781cbaa88809811d562499c5e5e7
ksume <ekin_keserer@msn.com>
2016-01-18 12:15:52 +0200

    Added placeholder data

--- src/data/sectionData.js
+++ src/data/sectionData.js
@@ -1,8 +1,12 @@
 app.value("sectionData", {
     "section_1": {
-        "text": "1",
-        "date": "September 2016",
-        "link": "",
+        "title": "Infrastructure for human enhancement",
+        "description": "Natural selection for your copy. Cilio " + 
+        "intelligently optimizes your writing for traction. Built " + 
+        "at a hackathon in late 2015, it has been a growing project " + 
+        "ever since, securing mentorship...",
+        "date": "NOV 2015",
+        "link": "GO TO SITE",
         "image_path": "",
         "type": "regular",
         "class": "section-1",
@@ -13,10 +17,14 @@ app.value("sectionData", {
         }
     },
     "section_2": {
-        "title": "2",
-        "description": "",
-        "date": "September 2016",
-        "link": "",
+        "title": "Commenting the web of music, a treatise",
+        "description": "Dunemoon came out of my fascination with the " +
+        "Spotify API, it might sound weird but thinking that something " + 
+        "like this would exist 20 years ago would be downright scifi. We " + 
+        "have tried to use this amazing opportunity to create a fun product " + 
+        "that Spotify users might appreciate.",
+        "date": "NOV 2015",
+        "link": "GO TO SITE",
         "image_path": "",
         "type": "regular",
         "class": "section-2",
@@ -27,14 +35,13 @@ app.value("sectionData", {
         }
     },
     "section_3": {
-        "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut " +
-        "labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut " +
-        "aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore" +
-        " eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt" +
-        " mollit anim id est laborum.",
-        "date": "November 2015",
-        "link": "BLABLA",
+        "title": "A small fix to the architect’s burden, behold...",
+        "description": "Cantilever. It is meant to solve a problem. An architect " +
+        "will suffer in NYC. But it’s also very rewarding, how do you balance the " + 
+        "two?  Let Cantilever ping you when you have some free time (if ever). Don’t " + 
+        "miss out on great events by reputable sources of Art and Design.",
+        "date": "JUN 2015",
+        "link": "GO TO SITE",
         "image_path": "",
         "type": "regular",
         "class": "section-3",
@@ -45,12 +52,17 @@ app.value("sectionData", {
         }
     },
     "section_4": {
-        "title": "4",
-        "description": "",
-        "date": "",
-        "link": "",
+        "title": "This one is for the good folks at KPCB",
+        "description": "Ever since I found out about it, " + 
+        "I’ve been eager to apply to the KPCB Fellows Program, " + 
+        "this year I made it to the deadline. If you are not from " + 
+        "over there but still want to check out a feature redesign for " + 
+        "Slack tag along!",
+        "date": "SEP 2015",
+        "link": "GO TO SITE",
         "image_path": "",
         "type": "regular",
+        "class": "section-4",
         "color": {
             "background": "#00adf2",
             "font": "white",
@@ -58,10 +70,14 @@ app.value("sectionData", {
         }
     },
     "section_5": {
-        "title": "5",
-        "description": "",
-        "date": "",
-        "link": "",
+        "title": "Rebranding the world’s first carbon fiber 3D Printer",
+        "description": "I’ve had the opportunity of interning as a designer " + 
+        "at Markforged last summer. The startup that’s building a 3d printer for " + 
+        "real world uses and serious prototyping rather than toys. During my internship " + 
+        "I did a lot of things, the most impactful of which is about to be released in " + 
+        "early 2016.",
+        "date": "SEP 2015",
+        "link": "GO TO SITE",
         "image_path": "",
         "type": "regular",
         "class": "section-5",
@@ -72,8 +88,15 @@ app.value("sectionData", {
         }
     },
     "section_6": {
-        "text": "1",
-        "type": "descriptive",
+        "title": "A puffy arrangement: building a hookah startup?",
+        "description": "Natural selection for your copy. Cilio " + 
+        "intelligently optimizes your writing for traction. Built " + 
+        "at a hackathon in late 2015, it has been a growing project " + 
+        "ever since, securing mentorship...",
+        "date": "SEP 2015",
+        "link": "GO TO SITE",
+        "image_path": "",
+        "type": "regular",
         "class": "section-6",
         "color": {
             "background": "white",
@@ -82,10 +105,13 @@ app.value("sectionData", {
         }
     },
     "section_7": {
-        "title": "2",
-        "description": "",
-        "date": "September 2016",
-        "link": "",
+        "title": "Horology and information design problems",
+        "description": "Natural selection for your copy. Cilio " + 
+        "intelligently optimizes your writing for traction. Built " + 
+        "at a hackathon in late 2015, it has been a growing project " + 
+        "ever since, securing mentorship...",
+        "date": "NOV 2015",
+        "link": "GO TO SITE",
         "image_path": "",
         "type": "regular",
         "class": "section-7",
@@ -96,12 +122,13 @@ app.value("sectionData", {
         }
     },
     "section_8": {
-        "title": "Lorem ipsum dolor sit amet, consectetur",
-        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut " +
-        "labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut " +
-        "aliquip ex ea commodo consequat.",
-        "date": "November 2015",
-        "link": "BLABLA",
+        "title": "Diagrams of quantified unquantifiables",
+        "description": "Natural selection for your copy. Cilio " + 
+        "intelligently optimizes your writing for traction. Built " + 
+        "at a hackathon in late 2015, it has been a growing project " + 
+        "ever since, securing mentorship...",
+        "date": "NOV 2015",
+        "link": "GO TO SITE",
         "image_path": "",
         "type": "regular",
         "class": "section-8",
@@ -112,10 +139,13 @@ app.value("sectionData", {
         }
     },
     "section_9": {
-        "title": "4",
-        "description": "",
-        "date": "",
-        "link": "",
+        "title": "Spatial neurotransmission of helical pseudonyms",
+        "description": "Natural selection for your copy. Cilio " + 
+        "intelligently optimizes your writing for traction. Built " + 
+        "at a hackathon in late 2015, it has been a growing project " + 
+        "ever since, securing mentorship...",
+        "date": "NOV 2015",
+        "link": "GO TO SITE",
         "image_path": "",
         "type": "regular",
         "class": "section-9",
@@ -126,10 +156,13 @@ app.value("sectionData", {
         }
     },
     "section_10": {
-        "title": "5",
-        "description": "",
-        "date": "",
-        "link": "",
+        "title": "A tale of three logos",
+        "description": "Natural selection for your copy. Cilio " + 
+        "intelligently optimizes your writing for traction. Built " + 
+        "at a hackathon in late 2015, it has been a growing project " + 
+        "ever since, securing mentorship...",
+        "date": "NOV 2015",
+        "link": "GO TO SITE",
         "image_path": "",
         "type": "regular",
         "class": "section-10",
@@ -140,10 +173,13 @@ app.value("sectionData", {
         }
     },
     "section_11": {
-        "title": "4",
-        "description": "",
-        "date": "",
-        "link": "",
+        "title": "A vacation house",
+        "description": "Natural selection for your copy. Cilio " + 
+        "intelligently optimizes your writing for traction. Built " + 
+        "at a hackathon in late 2015, it has been a growing project " + 
+        "ever since, securing mentorship...",
+        "date": "NOV 2015",
+        "link": "GO TO SITE",
         "image_path": "",
         "type": "regular",
         "class": "section-11",
@@ -154,10 +190,13 @@ app.value("sectionData", {
         }
     },
     "section_12": {
-        "title": "5",
-        "description": "",
-        "date": "",
-        "link": "",
+        "title": "The sharing economy of reprogrammable architecture",
+        "description": "Natural selection for your copy. Cilio " + 
+        "intelligently optimizes your writing for traction. Built " + 
+        "at a hackathon in late 2015, it has been a growing project " + 
+        "ever since, securing mentorship...",
+        "date": "NOV 2015",
+        "link": "GO TO SITE",
         "image_path": "",
         "type": "regular",
         "class": "section-12",
