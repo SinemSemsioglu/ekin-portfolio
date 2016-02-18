976bee501fc476e38425059695c457862932ba51
Sinem <sinem93@gmail.com>
2016-02-18 20:27:51 +0200

    added google analytics to index.html and fixed the path of resume link

--- src/data/sectionData.js
+++ src/data/sectionData.js
@@ -8,7 +8,7 @@ app.value("sectionData", {
         "<p>Currently: product design intern at <a href='http://markforged.com'><b>Markforged</b></a></p>" +
         "<p>I’ve co-created <a href='http://cil.io'><b>cil.io</b></a>.</p>" +
         "<br>" +
-        "<p>Here's my <a href='/assets/ekincvfebruary2.pdf'><b>resume</b></a>.</p></h1>",
+        "<p>Here's my <a href='assets/ekincvfebruary2.pdf'><b>resume</b></a>.</p></h1>",
         "type": "about",
         "class": "section-0",
         "color": {
